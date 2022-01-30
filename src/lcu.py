c3b2185eacc5c1e7054742dd90294cc2ed383ad5
Cássio Fontoura <cassiompf@gmail.com>
2022-01-29 23:06:02 -0400

    [FIX] access port for connect in lcu

--- src/lcu.py
+++ src/lcu.py
@@ -1,5 +1,6 @@
 import os
 
+
 class LcuInfo:
   def __init__(self):
     info_cmd = os.popen("wmic PROCESS WHERE name='RiotClientUx.exe' GET commandline").read()
@@ -10,4 +11,4 @@ class LcuInfo:
     index_after_app_port_label = info_cmd.find(app_port_label) + len(app_port_label)
     
     self.remoting_auth_token = info_cmd[index_after_remoting_auth_label:(index_after_remoting_auth_label + 22)]
-    self.access_port = int(info_cmd[index_after_app_port_label:(index_after_app_port_label + 5)])
+    self.access_port = info_cmd[index_after_app_port_label:(index_after_app_port_label + 5)].strip().replace('"', '')
